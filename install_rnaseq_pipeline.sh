40be8baeabd210c06d4e7bfa2022fdcf63ceddeb
Max Schelker <mschelker@merrimackpharma.com>
2015-11-04 18:33:54 -0500

    run sudo part without git

--- install_rnaseq_pipeline.sh
+++ install_rnaseq_pipeline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run sudo part of installation
+curl -fsSL https://github.com/leshaker/rnaseq_scripts/raw/master/install_rnaseq_pipeline_sudo.sh | bash
+
 # install custom pipeline scripts
 mkdir ~/RNAseq_pipeline
 cd ~/RNAseq_pipeline
@@ -8,15 +11,12 @@ chmod a+x ~/RNAseq_pipeline/rnaseq_scripts/*.sh
 mv ~/RNAseq_pipeline/rnaseq_scripts/*.sh ~/RNAseq_pipeline
 rm -rf rnaseq_scripts/
 
-# run sudo part of installation
-./install_rnaseq_pipeline_sudo.sh
-
 # assign user to docker group and reload groups
 sudo usermod -aG docker $USER
 newgrp docker
 
 # run user part of installation
-./install_rnaseq_pipeline_user.sh
+bash install_rnaseq_pipeline_user.sh
 
 # print completed message
 clear; echo -e '\n#######################\nInstallation completed!\n#######################\n'
