22014045667d60df3c62a2f594a9a16e85da4897
Max Schelker <mschelker@rnaseq2.mack.local>
2015-11-11 09:44:18 -0500

    added check if fastq file exists

--- download_data.sh
+++ download_data.sh
@@ -16,28 +16,34 @@ fname=$3
 fname=${fname%.*}
 
 if [ "$datasource" == "GEO" ]; then
-	echo "downloading data file ${fname}.fastq.gz"
-	fastq-dump --gzip --split-files -O data/ $id
-	rm -rf ~/ncbi 
+
+	# download fastq file if it doesn't exist
+	if [ ! -f "data/${fname}_1.fastq.gz" ] || [ ! -f "data/${fname}_2.fastq.gz" ]; then
+		echo "downloading data file ${fname}.fastq.gz"
+		fastq-dump --gzip --split-files -O data/ $id
+		rm -rf ~/ncbi 
+	fi
 
 	# rename files and write to GEO_index.txt
-	if [ -e "data/${id}_1.fastq.gz" ] && [ -e "data/${id}_2.fastq.gz" ]; then
+	# paired end
+	if [ -f "data/${id}_1.fastq.gz" ] && [ -f "data/${id}_2.fastq.gz" ]; then
 		mv data/${id}_1.fastq.gz data/${fname}_1.fastq.gz
 		mv data/${id}_2.fastq.gz data/${fname}_2.fastq.gz
 		echo -e "${fname}\t ${fname}\t data/${fname}_1.fastq.gz\t fastq FqRd1" >> GEO_index.txt
 		echo -e "${fname}\t ${fname}\t data/${fname}_2.fastq.gz\t fastq FqRd2" >> GEO_index.txt
-	elif [ -e "data/${id}_1.fastq.gz" ] && [ ! -e "data/${id}_2.fastq.gz" ]; then
+	# single end		
+	elif [ -f "data/${id}_1.fastq.gz" ] && [ ! -f "data/${id}_2.fastq.gz" ]; then
 		mv data/${id}_1.fastq.gz data/${fname}.fastq.gz
 		echo -e "${fname}\t ${fname}\t data/${fname}.fastq.gz\t fastq FqRd" >> GEO_index.txt
 	fi
 	echo -e "" >> GEO_index.txt
-
+	
 	echo "$fname successfully downloaded!"
 
 elif [ "$datasource" == "CCLE" ]; then
 
-	# download bam file if it doesn't exist
-	if [ ! -f "data/CCLE_${fname}.bam" ]; then
+	# download bam file if bam or fastq doesn't exist
+	if [ ! -f "data/CCLE_${fname}.bam" ] && ([ ! -f "data/CCLE_${fname}_1.fastq.gz" ] || [ ! -f "data/CCLE_${fname}_2.fastq.gz" ]); then
 		echo "downloading data file CCLE_${fname}.bam"
 		gtdownload --max-children 1 -k 0 -c cghub_public.key -v -d ${id} > logs/gt_${fname}.log 2> logs/gt_${fname}.err
 		mv ${id}/${fname}.bam data/CCLE_${fname}.bam
@@ -45,7 +51,8 @@ elif [ "$datasource" == "CCLE" ]; then
 		rm ${id}.gto
 	fi
 
-	if [[ ! -f "data/CCLE_${fname}_1.fastq.gz" || ! -f "data/CCLE_${fname}_2.fastq.gz" ]]; then
+	# convert bam to fastq
+	if [ ! -f "data/CCLE_${fname}_1.fastq.gz" ] || [ ! -f "data/CCLE_${fname}_2.fastq.gz" ]; then
 		echo "converting CCLE_${fname}.bam to fastq.gz"
 		./convert_bam_to_fastq.sh ${PWD}/data/CCLE_${fname}.bam
 	fi
