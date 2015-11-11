0627276f00ce24a15047bc5a5d67f0baa4dbcc69
Max Schelker <mschelker@merrimackpharma.com>
2015-11-11 10:07:37 -0500

    removed pipeline test from installation script and added logout

--- install_rnaseq_pipeline.sh
+++ install_rnaseq_pipeline.sh
@@ -6,8 +6,7 @@ curl -fsSL https://github.com/leshaker/rnaseq_scripts/raw/master/install_rnaseq_
 # run user part of installation
 curl -fsSL https://github.com/leshaker/rnaseq_scripts/raw/master/install_rnaseq_pipeline_user.sh | bash
 
-# run pipeline test
-./run_pipeline_test.sh
-
 # print completed message
 clear; echo -e '\n#######################\nInstallation completed!\n#######################\n'
+
+sleep 5; exit
