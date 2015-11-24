79133bfd8cd65ca7f16ba01e54e007c364f41a31
Max Schelker <mschelker@merrimack.com>
2015-11-24 15:18:10 -0500

    added transcript_id to gene_id conversion for kallisto

---
+++ transcripts2gene.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#input: transcript_expression
+#output: gene_expression
+
+input=$1
+output=$2
+
+join -1 1 -2 1 -o 1.2,2.4,2.5 -t$'\t' <(cat transcript_gene_table.txt | (read -r; printf "%s\n" "$REPLY"; sort)) <(cat $input | (read -r; printf "%s\n" "$REPLY"; sort)) \
+    | awk '(NR==1) {print $1,$2,$3} (NR >1) {n[$1];for(i=2;i<=NF;i++)a[$1,i]+=$i}END{for(x in n){printf "%s ", x;for(y=2;y<=NF;y++)printf "%s%s", a[x,y],(y==NF?ORS:OFS)}}' > $output
