5d2b16b3e94ad2d37c288525feee6bc48bbc844a
Max Schelker <mschelker@merrimack.com>
2015-11-25 11:19:17 -0500

    uses tab as delimiter and sorts output file

--- transcripts2gene.sh
+++ transcripts2gene.sh
@@ -5,9 +5,15 @@
 set -e
 
 input=$1
-output=$2
+if [ -z "$2" ]; then
+	output="genes_output.txt"
+else
+	output=$2
+fi
 
-join -1 1 -2 1 -o 1.2,2.4,2.5 -t$'\t' <(cat transcript_gene_table.txt | (read -r; printf "%s\n" "$REPLY"; sort)) <(cat $input | (read -r; printf "%s\n" "$REPLY"; sort)) \
-    | awk '(NR==1) {print $1,$2,$3} (NR >1) {n[$1];for(i=2;i<=NF;i++)a[$1,i]+=$i}END{for(x in n){printf "%s ", x;for(y=2;y<=NF;y++)printf "%s%s", a[x,y],(y==NF?ORS:OFS)}}' > $output
+join -1 1 -2 1 -o 1.2,2.4,2.5 -t $'\t' <(cat transcript_gene_table.txt | (read -r; printf "%s\n" "$REPLY"; sort)) <(cat $input | (read -r; printf "%s\n" "$REPLY"; sort)) \
+    | awk '(NR==1) {print $1,$2,$3} (NR >1) {n[$1];for(i=2;i<=NF;i++)a[$1,i]+=$i}END{for(x in n){printf "%s\t", x;for(y=2;y<=NF;y++)printf "%s%s", a[x,y],(y==NF?ORS:OFS)}}' OFS='\t' \
+	| (read -r; printf "%s\n" "$REPLY"; sort) \
+    > $output
 
 exit 0
